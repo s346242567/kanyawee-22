027cb81d1e9040366211949acac2216e1fe604bd
s346242567 <s34624@banbung.ac.th>
2026-06-22 11:27:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
-document.addEventListener('DOMContentLoaded',function ()
-   condole.log('Portfolio ของ KANYAWEE พร้อมแล้ว!');
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Portfolio ของ KANYAWEE พร้อมแล้ว!');
 });
